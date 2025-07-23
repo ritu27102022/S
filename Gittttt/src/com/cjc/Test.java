e057ae9da5b7eba8c1e973ad60264cd6e6675e0f
LENOVO <LENOVO@DESKTOP-NMF9K3U>
2025-07-23 13:21:38 +0530

    project display in Git

---
+++ Gittttt/src/com/cjc/Test.java
@@ -0,0 +1,20 @@
+package com.cjc;
+
+public class Test 
+{
+	public void add()
+	{
+		System.out.println("this is additon method");
+	}
+    
+	public void mul()
+	{
+		System.out.println("This is multiplication method");
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("This is main method");
+		
+	}
+}
